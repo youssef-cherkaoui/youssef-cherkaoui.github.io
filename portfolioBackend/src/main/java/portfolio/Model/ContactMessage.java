4411d423418cebf27d8bc2af203e4a2d3b52011e
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2024-12-07 02:29:29 +0100

    Backend Portfolio

---
+++ portfolioBackend/src/main/java/portfolio/Model/ContactMessage.java
@@ -0,0 +1,28 @@
+package portfolio.Model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ContactMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String message;
+    private String email;
+
+}
