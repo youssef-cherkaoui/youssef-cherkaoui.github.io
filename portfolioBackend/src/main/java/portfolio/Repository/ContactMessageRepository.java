d1c5a899fb60b16332c1f96bda9e12167752b079
youssef-cherkaoui <youssefessaddik.cherkaoui@gmail.com>
2024-12-12 12:05:24 +0100

    version

--- portfolioBackend/src/main/java/portfolio/Repository/ContactMessageRepository.java
+++
@@ -1,9 +0,0 @@
-package portfolio.Repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import portfolio.Model.ContactMessage;
-
-public interface ContactMessageRepository extends JpaRepository<ContactMessage, Long> {
-
-}
